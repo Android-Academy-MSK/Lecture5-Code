2b6b650edf06e63bda9eec066e506613c7964a02
Alex Bykov <me@alexbykov.ru>
2018-11-25 23:50:48 +0300

    fixed strategies

--- app/src/main/java/ru/alexbykov/networksample/giflist/mvp/GifListView.java
+++ app/src/main/java/ru/alexbykov/networksample/giflist/mvp/GifListView.java
@@ -3,8 +3,7 @@ package ru.alexbykov.networksample.giflist.mvp;
 import android.support.annotation.NonNull;
 
 import com.arellomobile.mvp.MvpView;
-import com.arellomobile.mvp.viewstate.strategy.AddToEndStrategy;
-import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 import java.util.List;
@@ -13,11 +12,10 @@ import ru.alexbykov.networksample.data.network.dto.GifDTO;
 import ru.alexbykov.networksample.giflist.common.State;
 
 
-@StateStrategyType(AddToEndStrategy.class)
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface GifListView extends MvpView {
 
 
-    @StateStrategyType(OneExecutionStateStrategy.class)
     void showData(@NonNull List<GifDTO> gifs);
 
     void showState(@NonNull State state);
